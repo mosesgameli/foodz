b97dc36703188db1d01789d558431f35f097a57e
mosesgameli <mosesgameli20@gmail.com>
2024-04-10 21:34:45 +0000

    chore: initialize project

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+export default function Page() {
+  return (
+    <div className="">
+      <nav className="flex flex-row">
+        <span className="font-bold text-xl">Foodz</span>
+      </nav>
+    </div>
+  );
+}
